2f4bf163a6006631c726f2b738fd9d2d82016e3e
DevOps <infraestrutura.devops@gmail.com>
2021-01-18 18:27:17 -0300

    Update registro

--- ponto.py
+++ ponto.py
@@ -17,18 +17,16 @@ driver.get("https://app.pontomaisweb.com.br/#/acessar")
 def ponto():
     driver.find_element_by_name('login').send_keys(username)
     driver.find_element_by_name('password').send_keys(password)
-
+    print('Login')
     logar = driver.find_element_by_xpath("/html/body/ng-view/div/div/form/div/div[4]/div/div/div[3]/button")
     logar.click()
     time.sleep(20)
     
     registrar = driver.find_element_by_xpath('//*[@id="content-wrapper"]/div[2]/div/ng-view/div[2]/button')
     driver.execute_script("arguments[0].click();", registrar)
+    registrar.click()
     print(f'Ponto registrado para: {username}')
-    
-    #button = driver.find_element_by_class_name("text-center mt15 ng-scope")
-    #button.click()
-    
+
     ok = driver.find_element_by_class_name("modal-body pm-alert-body ng-scope")
     ok.click()
 
