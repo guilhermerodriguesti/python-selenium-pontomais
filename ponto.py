ed913c3f19be3ba3d83abe7e85b22d1ca1eaae63
DevOps <infraestrutura.devops@gmail.com>
2021-01-18 10:59:24 -0300

    fix: corrige registro de ponto

--- ponto.py
+++ ponto.py
@@ -1,27 +1,33 @@
 import time
+import sys
+import os
 from selenium import webdriver
+from dotenv import load_dotenv
 from selenium.webdriver.common.keys import Keys
 
-USERNAME = 'user@email.com'
-PASSWORD = 'pass'
+# Credentials
+load_dotenv('.env')
+
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
 
 driver = webdriver.Chrome()
 driver.get("https://app.pontomaisweb.com.br/#/acessar")
 
-
 def ponto():
-    usuario = driver.find_element_by_name('login')
-    usuario.send_keys(USERNAME)
-
-    senha = driver.find_element_by_name('password')
-    senha.send_keys(PASSWORD)
+    driver.find_element_by_name('login').send_keys(username)
+    driver.find_element_by_name('password').send_keys(password)
 
     logar = driver.find_element_by_xpath("/html/body/ng-view/div/div/form/div/div[4]/div/div/div[3]/button")
     logar.click()
     time.sleep(20)
     
-    registrar = driver.find_element_by_xpath('//*[@id="content-wrapper"]/div[2]/div/ng-view/div[2]/button')
-    driver.execute_script("arguments[0].click();", registrar)
+    #registrar = driver.find_element_by_xpath('//*[@id="content-wrapper"]/div[2]/div/ng-view/div[2]/button')
+    #driver.execute_script("arguments[0].click();", registrar)
+    
+    #driver.find_element_by_css_selector("a[ng-click='Registrar ponto']").click()
+    #button = driver.find_element_by_class_name("text-center mt15 ng-scope")
+    #button.click()
     
     driver.quit()
 
