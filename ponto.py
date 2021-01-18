30240a6be794ed57232118c6abc440f05d57a2bc
DevOps <infraestrutura.devops@gmail.com>
2021-01-18 11:01:20 -0300

    fix: corrige registro de ponto

--- ponto.py
+++ ponto.py
@@ -22,12 +22,8 @@ def ponto():
     logar.click()
     time.sleep(20)
     
-    #registrar = driver.find_element_by_xpath('//*[@id="content-wrapper"]/div[2]/div/ng-view/div[2]/button')
-    #driver.execute_script("arguments[0].click();", registrar)
-    
-    #driver.find_element_by_css_selector("a[ng-click='Registrar ponto']").click()
-    #button = driver.find_element_by_class_name("text-center mt15 ng-scope")
-    #button.click()
+    registrar = driver.find_element_by_xpath('//*[@id="content-wrapper"]/div[2]/div/ng-view/div[2]/button')
+    driver.execute_script("arguments[0].click();", registrar)
     
     driver.quit()
 
